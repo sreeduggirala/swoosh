e515dce2cb91d73e4dd5a697710f05fcb749f5cb
Ian Kim <iankim27@utexas.edu>
2024-03-01 02:47:21 -0700

    thirdweb integration

--- web/app/home/page.tsx
+++ web/app/home/page.tsx
@@ -4,42 +4,20 @@ import { useAccount, useReadContract } from 'wagmi';
 import HomeHeader from '../components/HomeHeader';
 import HomeGroup from '../components/HomeGroup';
 import { readSwooshContract } from 'app/util';
-
-interface Request {
-  id: string;
-  creditor: string;
-  debtors: string[];
-  paid: any[]; // Adjust the type according to what `paid` actually contains
-  declined: any[]; // Same here, adjust the type as necessary
-  amount: string;
-  message: string;
-  imageURI: string;
-  timestamp: string;
-  fulfilled: boolean;
-  cancelled: boolean;
-}
-
+import { ThirdwebProvider, useAddress } from '@thirdweb-dev/react';
+import { BaseSepoliaTestnet } from '@thirdweb-dev/chains';
+import Wrapper from './wrapper';
 export default function HomePage() {
-  const user_address = useAccount().address;
-  // console.log(user_address);
-  const [resultOut, setResultOut] = useState<Request[]>([]);
-  const [resultIn, setResultIn] = useState<Request[]>([]);
-
-  let result = readSwooshContract('getRequestsOut', [user_address], setResultOut);
-  result = readSwooshContract('getRequestsIn', [user_address], setResultIn);
-
-  if (result.isLoading) return <p>Loading ...</p>;
-  if (result.error) return <p>Error: {result.error.message}</p>;
 
   return (
     <div className="px-4 py-8">
-      <HomeHeader />
-      <HomeGroup inNumber={resultIn.length} outNumber={resultOut.length} />
+    <ThirdwebProvider
+    activeChain={BaseSepoliaTestnet}
+      clientId="3524eeab46d7c262cb23bcf072d92d5e"
+    >
+      <Wrapper></Wrapper>
+      </ThirdwebProvider>
     </div>
   );
 
-  interface HomeGroupProp {
-    inNumber: number;
-    outNumber: number;
-  }
 }   
