c0ad3485e5d3e09347932c5c03c23b1b2abe9f71
Ian Kim <iankim27@utexas.edu>
2024-03-01 13:33:14 -0700

    start thirdweb

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { generateMetadata } from '@/utils/generateMetadata';
 import SignInPage from './sign-in';
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, embeddedWallet, smartWallet } from "@thirdweb-dev/react";
 import { BaseSepoliaTestnet } from "@thirdweb-dev/chains";
 import { base } from 'viem/chains';
 
@@ -24,6 +24,16 @@ export default function Page() {
     <ThirdwebProvider
     activeChain={BaseSepoliaTestnet}
       clientId="3524eeab46d7c262cb23bcf072d92d5e"
+      supportedWallets={[
+        smartWallet(
+          embeddedWallet(), // any personal wallet
+          {
+            factoryAddress: "0xFB5dA66aE989c5B1926a70107c9c8a75D5e5cEa5", // your deployed factory address
+            gasless: true, // enable or disable gasless transactions
+          },
+        ), 
+      ]}
+
     >
       <SignInPage />
     </ThirdwebProvider>
--- web/app/profile/Wrapper.tsx
+++ web/app/profile/Wrapper.tsx
@@ -10,11 +10,11 @@ import { useAccount } from 'wagmi';
 import profile_icon from '../assets/logo.png';
 import Image from 'next/image';
 import { ThirdwebProvider, embeddedWallet, smartWallet, useAddress } from '@thirdweb-dev/react';
-import Wrapper from "./Wrapper";
 import {BaseSepoliaTestnet} from '@thirdweb-dev/chains';
-const ProfilePagProfilePagee = () => {
 
-  // const user_address =  useAddress();
+const Wrapper = () => {
+
+  const user_address =  useAddress();
   // const router = useRouter();
 
   // useEffect(() => {
@@ -24,25 +24,14 @@ const ProfilePagProfilePagee = () => {
   // }, [user_address]);
 
   return (
-    // <div className="flex flex-col px-4 pb-28 h-screen overflow-y-hidden rounded-sm">
-    //   <div className="sticky top-0 z-10 h-full pb-28 bg-white">
-    //     <Header title="Profile" />
-    //     <div className='w-full flex justify-center'>
-    //       <Image src={profile_icon} alt="profile picture" width={100} height={100} className='pb-4' />
-    //     </div>
-    //     <div className='w-full  flex flex-col space-y-8'>
-    //       <ProfileBalanceCard />
-    //       {/* <div className=" my-4 m-auto max-w-xs rounded-lg bg-gradient-to-br from-blue-200 to-blue-300 p-4 px-8 sm:max-w-md"> */}
-    //       {/* {user_address != undefined ? 
-    //           <AccountInfoPanel /> : <div></div>
-    //         } */}
-    //       {/* </div> */}
-    //     </div>
-        
-    //   </div>
-    // </div>
-    <div>
-              <ThirdwebProvider
+    <div className="flex flex-col px-4 pb-28 h-screen overflow-y-hidden rounded-sm">
+      <div className="sticky top-0 z-10 h-full pb-28 bg-white">
+        <Header title="Profile" />
+        <div className='w-full flex justify-center'>
+          <Image src={profile_icon} alt="profile picture" width={100} height={100} className='pb-4' />
+        </div>
+        <div className='w-full  flex flex-col space-y-8'>
+        <ThirdwebProvider
     activeChain={BaseSepoliaTestnet}
       clientId="3524eeab46d7c262cb23bcf072d92d5e"
       supportedWallets={[
@@ -56,10 +45,19 @@ const ProfilePagProfilePagee = () => {
       ]}
 
     >
-      <Wrapper />
-      </ThirdwebProvider>
+          <ProfileBalanceCard />
+          {/* <div className=" my-4 m-auto max-w-xs rounded-lg bg-gradient-to-br from-blue-200 to-blue-300 p-4 px-8 sm:max-w-md"> */}
+          {/* {user_address != undefined ? 
+              <AccountInfoPanel /> : <div></div>
+            } */}
+          {/* </div> */}
+          </ThirdwebProvider>
+
+        </div>
+        
+      </div>
     </div>
   );
 };
 
-export default ProfilePagProfilePagee;
+export default Wrapper;
