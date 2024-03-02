282d1a9a28b82c865776f57782b1e8be62e5cbd0
Ian Kim <iankim27@utexas.edu>
2024-03-02 03:58:28 -0700

    hee hee

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,7 +5,7 @@ import { ThirdwebProvider, embeddedWallet, smartWallet } from "@thirdweb-dev/rea
 import { BaseSepoliaTestnet } from "@thirdweb-dev/chains";
 import { base } from 'viem/chains';
 import {AnimatePresence, motion} from 'framer-motion'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 // export const metadata = generateMetadata({
 //   title: 'Build Onchain Apps Toolkit',
@@ -26,11 +26,8 @@ export default function Page() {
 
   return (
     
-<AnimatePresence exitBeforeEnter  >
-  <motion.div key={router.route} initial={{opacity:0}} animate={{opacity:1}} transition={{ease:'easeIn', duration:2}}>
       <SignInPage />
-      </motion.div>
-      </AnimatePresence>
+   
    
     
   );
--- web/app/sign-in/index.tsx
+++ web/app/sign-in/index.tsx
@@ -1,5 +1,5 @@
 import AccountConnect from '@/components/layout/header/AccountConnect';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { ConnectWallet, useAddress, useContract, useContractWrite, useContractRead} from "@thirdweb-dev/react";
 
@@ -7,15 +7,28 @@ export default function SignInPage() {
   const router = useRouter();
   const address = useAddress();
   const { contract } = useContract(process.env.ERC20_ADDRESS); 
+  let [userAllowance, setUserAllowance] = useState<number>();
   const { mutateAsync, isLoading: approve_isLoading, error: approve_error } = useContractWrite(
     contract,
     "approve",
   );
   const {data, isLoading: allowance_isLoading, error: allowance_error} = useContractRead(contract,'allowance',[address, process.env.CONTRACT_ADDRESS])
 
+  useEffect(() => {
+    if (address != undefined) {
+      contract?.call("allowance", [address, process.env.CONTRACT_ADDRESS]).then((data)=> {
+        // alert(data);
+        setUserAllowance(data);
+      });
+    }
+  }, [address]);
+  // useEffect
+  if (address != null && userAllowance as number > 0) {  
+    router.push('/home');
+  }
   const handleApprove = () => {
     mutateAsync({args:[process.env.CONTRACT_ADDRESS, "10000000000000000000000"]}).then(data=>{
-      if (address != null && contract != null && data.receipt != undefined) {  
+      if (address != null && userAllowance as number > 0) {  
         router.push('/home');
       }
     })
@@ -26,7 +39,7 @@ export default function SignInPage() {
       <h1 className=" mb-32 text-6xl tracking-widest text-blue-100 ">SWOOSH</h1>
         <div className="w-64 flex flex-col justify-center gap-2 ">
         <ConnectWallet  modalSize='wide' hideSwitchToPersonalWallet={true} hideSendButton={true} hideReceiveButton={true}/>
-        {(address != null) ?  <button className="  btn btn-square btn-primary bg-blue-100 text-blue-300 btn-wide" disabled={approve_isLoading} onClick={handleApprove}> {approve_isLoading ?   <span className="loading loading-spinner"/> :"Approve USDC"}</button> : null}
+        {(address != null && userAllowance as number == 0) ?  <button className="  btn btn-square btn-primary bg-blue-100 text-blue-300 btn-wide" disabled={approve_isLoading} onClick={handleApprove}> {approve_isLoading ?   <span className="loading loading-spinner"/> :"Approve USDC"}</button> : null}
       </div>
     </div>
   );   
